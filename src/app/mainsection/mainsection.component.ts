1545bd1e136fb0f45bab65691e5e96473b75a1c0
Feliciter <padakluev@gmx.com>
2020-08-04 00:43:48 +0300

    add: view info from service

--- src/app/mainsection/mainsection.component.ts
+++ src/app/mainsection/mainsection.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../shared/api.service';
+ 
+ 
 @Component({
   selector: 'app-mainsection',
   templateUrl: './mainsection.component.html',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainsectionComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  webimage;
+  title;
+  description;
+  category;
+  tags;
+  addinfo;
+  datasrc
+
+
+	constructor(private apiService: ApiService) 
+  { }
+
+  ngOnInit() {
+		this.apiService.fetchData().subscribe((data: any[])=>{  
+			console.log(data['artObjects']);  
+    	this.datasrc = data['artObjects'];
+    
+    
+		})  
+	}
 
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -13,52 +13,58 @@ export class ApiService {
   endpoint =  environment.apiUrl; 
   keyapi =  environment.keyApi; 
 
+  private SERVER_URL = "https://www.rijksmuseum.nl/api/nl/collection?key=6bTGB81i&involvedMaker=Rembrandt+van+Rijn";
+
   headers = new HttpHeaders()
   .set('Content-Type', 'application/json')
  
 
-   params = new HttpParams()
-  .set('key',this.keyapi);
+  //  params = new HttpParams()
+  // .set('key',this.keyapi);
 
   constructor(private http: HttpClient) { }
 
+  public fetchData(){  
+		return this.http.get(`${this.SERVER_URL}`);  
+	}  
+
  //https://www.rijksmuseum.nl/api/nl/collection?key=6bTGB81i&involvedMaker=Rembrandt+van+Rijn
   // Get Collection 
-  GetCollection() {
-    return this.http.get(`${this.endpoint},{params}`);
-  }
+  // GetCollection() {
+  //   return this.http.get(`${this.endpoint},{params}`);
+  // }
 
 
 
   // Get CollectionDetails
-  GetCollectionDetails(id): Observable<any> {
-    let API_URL = `${this.endpoint}/read-recipe/${id}`;
-    return this.http.get(API_URL, { headers: this.headers })
-      .pipe(
-        map((res: Response) => {
-          return res || {}
-        }),
-        catchError(this.errorMgmt)
-      )
-  }
+  // GetCollectionDetails(id): Observable<any> {
+  //   let API_URL = `${this.endpoint}/read-recipe/${id}`;
+  //   return this.http.get(API_URL, { headers: this.headers })
+  //     .pipe(
+  //       map((res: Response) => {
+  //         return res || {}
+  //       }),
+  //       catchError(this.errorMgmt)
+  //     )
+  //}
 
 
 
  
 
   // Error handling 
-  errorMgmt(error: HttpErrorResponse) {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      // Get client-side error
-      errorMessage = error.error.message;
-    } else {
-      // Get server-side error
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-    }
+  // errorMgmt(error: HttpErrorResponse) {
+  //   let errorMessage = '';
+  //   if (error.error instanceof ErrorEvent) {
+  //     // Get client-side error
+  //     errorMessage = error.error.message;
+  //   } else {
+  //     // Get server-side error
+  //     errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+  //   }
     
-    return throwError(errorMessage);
-  }
+  //   return throwError(errorMessage);
+  // }
 
 
 }
